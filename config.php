fdfd36edb18708a3cf502778f8dc568439979b73
NaijaProgrammer <orji4y@yahoo.com>
2017-01-21 07:31:29 +0100

    Updated config allowed CORS domains

--- config.php
+++ config.php
@@ -5,4 +5,4 @@ $user_pass     = '';
 $db_name       = 'codementor';
 $tables_prefix = 'cm_';
 $dir_url       = 'http://localhost/sites/codementor/chat';
-$allowed_cors_domains = array('localhost', '127.0.0.1');
+$allowed_cors_domains = array('localhost', '127.0.0.1', '192.168.1.1');
